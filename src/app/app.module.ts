ca8e08b48151886ebff93e064b8ca17afd2ffb46
freefireman <qwrtui132>
2020-11-26 17:44:15 +0200

    added all lectures, about us & login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,35 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { LoginPageComponent } from './login-page/login-page.component';
 import {FormsModule} from "@angular/forms";
+import { AboutUsComponent } from './about-us/about-us.component';
+import {RouterModule} from "@angular/router";
+import { AllLecturesComponent } from './all-lectures/all-lectures.component';
+
+const routers = [
+  {
+    path: 'about-us', component: AboutUsComponent
+  },
+  {
+    path: 'lectures', component: AllLecturesComponent
+  },
+  {
+    path: 'login', component: LoginPageComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    AboutUsComponent,
+    AllLecturesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routers)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
