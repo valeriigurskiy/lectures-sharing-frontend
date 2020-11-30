de571e9c2889aaa62ff9aeecfc7d49ebf16e17ee
ftplz <ya.zazaza77@gmail.com>
2020-11-30 19:36:07 +0200

    added validators to registration page, and some little changes

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpRequest} from '@angular/common/http';
 import {User} from '../entity/User';
 import {University} from '../entity/University';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Title} from '@angular/platform-browser';
 interface Food {
   value: string;
@@ -15,25 +15,31 @@ interface Food {
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor(private httpClient: HttpClient, private title: Title) {
+  constructor(private httpClient: HttpClient, private title: Title, private formBuilder: FormBuilder) {
     this.title.setTitle('Registration');
   }
 
   user: User;
-  validatingForm: FormGroup;
+  myGroup: FormGroup;
+  profileForm: FormGroup;
   universities: University;
   value: University;
   passwordValue: boolean;
+  buttonStatus: true;
+  dateObj = new Date();
 
   ngOnInit(): void {
-    this.validatingForm = new FormGroup({
-      login: new FormControl(),
-      password: new FormControl(),
-      name: new FormControl(),
-      lastname: new FormControl(),
-      age: new FormControl()
+    this.myGroup = new FormGroup({
+      login: new FormControl('', Validators.minLength(4)),
+      password: new FormControl('', Validators.minLength(6)),
+      name: new FormControl('', Validators.minLength(2)),
+      lastname: new FormControl('', Validators.minLength(2)),
+      age: new FormControl('', Validators.min(16)),
+      university: new FormControl('', Validators.required)
     });
+
     this.httpClient.get<University>('http://localhost:8080/university').subscribe(value => this.universities = value);
+    console.log(this.value);
   }
 
   passwordTrigger(event): void{
@@ -44,12 +50,14 @@ export class RegistrationComponent implements OnInit {
     }
   }
 
-  saveUser(login: string, password: string, name: string, lastname: string, age: string, role: string): void{
-    const university = this.value.name;
+  saveUser(login: string, password: string, name: string, lastname: string, age: string, university: string, role: string): void{
+    if (age > '30'){
+      age = '30';
+    }
+
     const body = {login, password, name, lastname, age, university, role};
     const req = new HttpRequest('POST', 'http://localhost:8080/users', body);
     this.httpClient.request(req).subscribe(value => console.log(value));
-    console.log(university);
   }
-
 }
+
